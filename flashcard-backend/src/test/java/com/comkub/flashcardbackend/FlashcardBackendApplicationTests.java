11bfe2d26ac75cfe92c4e82fbdb5c5d9385bb6af
chaxyz <chaiithep2546@gmail.com>
2024-06-18 18:27:31 +0700

    setup

---
+++ flashcard-backend/src/test/java/com/comkub/flashcardbackend/FlashcardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comkub.flashcardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
